0e9adb6806ac80a3330199bca33d0e36972afc00
Peter Fields <pcfields@gmail.com>
2022-07-02 21:49:22 -0400

    remove plugins to start over

--- lua/peterf/plugins.lua
+++ lua/peterf/plugins.lua
@@ -36,59 +36,64 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-    -- My plugins here
-    use "wbthomason/packer.nvim" -- Have packer manage itself
-    use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-    use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-    use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-    use "numToStr/Comment.nvim" -- Easily comment stuff
-    use "kyazdani42/nvim-web-devicons"
-    use "kyazdani42/nvim-tree.lua"
-    use "akinsho/bufferline.nvim"
-    use "moll/vim-bbye"
-    use "nvim-lualine/lualine.nvim"
-    use "akinsho/toggleterm.nvim"
-    use "ahmedkhalf/project.nvim"
-    use "lewis6991/impatient.nvim"
-    use "lukas-reineke/indent-blankline.nvim"
-    use "goolord/alpha-nvim"
-    use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
-    use "folke/which-key.nvim"
 
     -- Colorschemes
-    --  use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
     use 'sainnhe/gruvbox-material'
 
-    -- cmp plugins
-    use "hrsh7th/nvim-cmp" -- The completion plugin
-    use "hrsh7th/cmp-buffer" -- buffer completions
-    use "hrsh7th/cmp-path" -- path completions
-    use "hrsh7th/cmp-cmdline" -- cmdline completions
-    use "saadparwaiz1/cmp_luasnip" -- snippet completions
-    use "hrsh7th/cmp-nvim-lsp"
-
-    -- snippets
-    use "L3MON4D3/LuaSnip" -- snippet engine
-    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-
-    -- LSP
-    use "neovim/nvim-lspconfig" -- enable LSP
-    use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-    use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-    use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+    -- Plugins
+    use "wbthomason/packer.nvim" -- Have packer manage itself
+    use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
+    -- use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
 
     -- Telescope
-    use "nvim-telescope/telescope.nvim"
+    use {
+        'nvim-telescope/telescope.nvim', -- The main Telescope plugin  
+        requires = {{'nvim-lua/plenary.nvim'}}
+    }
+    -- use "nvim-telescope/telescope-fzf-native.nvim" -- An optional plugin recommended by Telescope docs'
+
+    -- NVIM Tree here
+    use 'kyazdani42/nvim-tree.lua'
+    use 'kyazdani42/nvim-web-devicons' -- optional, for file icons
 
     -- Treesitter
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate"
-    }
-    use "JoosepAlviste/nvim-ts-context-commentstring"
+    use "nvim-treesitter/nvim-treesitter"
+
+    -- use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
+    -- use "numToStr/Comment.nvim" -- Easily comment stuff
+    -- use "akinsho/bufferline.nvim"
+    -- use "moll/vim-bbye"
+    -- use "nvim-lualine/lualine.nvim"
+    -- use "akinsho/toggleterm.nvim"
+    -- use "ahmedkhalf/project.nvim"
+    -- use "lewis6991/impatient.nvim"
+    -- use "lukas-reineke/indent-blankline.nvim"
+    -- use "goolord/alpha-nvim"
+    -- use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
+    -- use "folke/which-key.nvim"
+
+    -- cmp plugins
+    -- use "hrsh7th/nvim-cmp" -- The completion plugin
+    -- use "hrsh7th/cmp-buffer" -- buffer completions
+    -- use "hrsh7th/cmp-path" -- path completions
+    -- use "hrsh7th/cmp-cmdline" -- cmdline completions
+    -- use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    -- use "hrsh7th/cmp-nvim-lsp"
+
+    -- snippets
+    -- use "L3MON4D3/LuaSnip" -- snippet engine
+    -- use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+    -- LSP
+    -- use "neovim/nvim-lspconfig" -- enable LSP
+    -- use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+    -- use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+    -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+
+    -- use "JoosepAlviste/nvim-ts-context-commentstring"
 
     -- Git
-    use "lewis6991/gitsigns.nvim"
+    -- use "lewis6991/gitsigns.nvim"
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
