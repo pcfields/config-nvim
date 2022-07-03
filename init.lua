67b1a9c706ace7362fca4135f85af28ab8816c72
Peter Fields <pcfields@gmail.com>
2022-07-02 21:42:14 -0400

    adjust plugins included

--- init.lua
+++ init.lua
@@ -1,7 +1,11 @@
+-- Settings
+require "peterf.colorscheme"
 require "peterf.options"
 require "peterf.keymaps"
+
+-- Plugins
 require "peterf.plugins"
-require "peterf.colorscheme"
-require "peterf.cmp"
-require "peterf.lsp"
+require "peterf.telescope"
+require "peterf.treesitter"
+require "peterf.nvim-tree"
 
