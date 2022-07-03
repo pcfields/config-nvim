2478caa503cc89767670f864e2ab2b01381ed381
Peter Fields <pcfields@gmail.com>
2022-07-02 21:48:19 -0400

    fix leader key not working

--- lua/peterf/options.lua
+++ lua/peterf/options.lua
@@ -9,7 +9,7 @@ local options = {
     ignorecase = true, -- ignore case in search patterns
     mouse = "a", -- allow the mouse to be used in neovim
     pumheight = 10, -- pop up menu height
-    showmode = false, -- we don't need to see things like -- INSERT -- anymore
+    showmode = true, -- we don't need to see things like -- INSERT -- anymore
     showtabline = 2, -- always show tabs
     smartcase = true, -- smart case
     smartindent = true, -- make indenting smarter again
@@ -17,7 +17,6 @@ local options = {
     splitright = true, -- force all vertical splits to go to the right of current window
     swapfile = false, -- creates a swapfile
     termguicolors = true, -- set term gui colors (most terminals support this)
-    timeoutlen = 100, -- time to wait for a mapped sequence to complete (in milliseconds)
     undofile = true, -- enable persistent undo
     updatetime = 300, -- faster completion (4000ms default)
     writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
@@ -32,7 +31,8 @@ local options = {
     wrap = false, -- display lines as one long line
     scrolloff = 8, -- is one of my fav
     sidescrolloff = 8,
-    guifont = "monospace:h17" -- the font used in graphical neovim applications
+    guifont = "monospace:h17", -- the font used in graphical neovim applications
+    backupcopy = yes
 }
 
 vim.opt.shortmess:append "c"
@@ -43,4 +43,3 @@ end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
