0a89f4ddfa770fc49c9f2f3f6cc922c4bdeca4fb
Peter Fields <pcfields@gmail.com>
2022-07-03 22:33:52 -0400

    add npm packages to install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Install for eslint, json, css, html support
+npm i -g vscode-langservers-extracted  
+
+# Install typescript language server
+npm install -g typescript typescript-language-server
+
+# Install Elm server
+npm install -g elm elm-test elm-format @elm-tooling/elm-language-server
