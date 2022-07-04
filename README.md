43e722b9f867fe2bba0b443e47f19fc5b2a621a5
Peter Fields <pcfields@gmail.com>
2022-07-03 22:40:13 -0400

    update install and readme

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ My Neovim config
 
 Inspired by:
 
+- https://www.jakewiesler.com/blog
 - https://github.com/vuki656/nvim-config
 - https://github.com/LunarVim/nvim-basic-ide
 - https://github.com/LunarVim/Neovim-from-scratch
--- install.sh
+++ install.sh
@@ -1,3 +1,28 @@
+# Install nix package manager
+curl -L https://nixos.org/nix/install | sh
+
+# Source nix
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages
+nix-env -iA \
+	nixpkgs.zsh \
+	nixpkgs.git \
+	nixpkgs.neovim \
+	nixpkgs.tmux \
+	nixpkgs.fzf \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.direnv \
+	nixpkgs.stow \
+    nixpkgs.fd \
+    nixpkgs.gcc \
+    nixpkgs.gnumake \
+
+# Install Volta
+curl https://get.volta.sh | bash
+volta install node
+
 # Install for eslint, json, css, html support
 npm i -g vscode-langservers-extracted  
 
