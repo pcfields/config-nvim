ad355be2e13862833af2904ac3ac3eae2c375ead
Peter Fields <pcfields@gmail.com>
2022-07-06 19:59:49 -0400

    update lualine icon separators

--- lua/peterf/plugins/lualine.lua
+++ lua/peterf/plugins/lualine.lua
@@ -1,1 +1,17 @@
-require('lualine').setup()
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = {
+            left = '',
+            right = ''
+        },
+        section_separators = {
+            left = '',
+            right = ''
+        },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = false
+    }
+}
