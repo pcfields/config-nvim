bc20f754e09190b5a0458b9187dee74fe42a617d
Peter Fields <pcfields@gmail.com>
2022-07-06 20:54:24 -0400

    add treesitter refactor

--- lua/peterf/plugins-setup.lua
+++ lua/peterf/plugins-setup.lua
@@ -25,6 +25,7 @@ return packer.startup(function(use)
     use "wbthomason/packer.nvim" -- Have packer manage itself
     use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
     use 'romgrk/barbar.nvim' -- styled tabs
+
     -- Telescope
     use {
         'nvim-telescope/telescope.nvim', -- The main Telescope plugin  
@@ -41,6 +42,7 @@ return packer.startup(function(use)
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
+    use 'nvim-treesitter/nvim-treesitter-refactor'
 
     use {
         'nvim-lualine/lualine.nvim',
--- lua/peterf/plugins/treesitter.lua
+++ lua/peterf/plugins/treesitter.lua
@@ -6,5 +6,22 @@ treesitter.setup {
                         "json", "markdown", "scss", "tsx", "vim"},
     highlight = {
         enable = true
+    },
+    refactor = {
+        highlight_current_scope = {
+            enable = true
+        },
+        smart_rename = {
+            enable = true,
+            keymaps = {
+                smart_rename = "rr"
+            }
+        },
+        highlight_definitions = {
+            enable = true,
+            -- Set to false if you have an `updatetime` of ~100.
+            clear_on_cursor_move = true
+        }
     }
+
 }
