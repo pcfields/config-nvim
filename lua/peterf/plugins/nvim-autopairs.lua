c89997b01818fc65385b14e16231cb90079d92c2
Peter Fields <pcfields@gmail.com>
2022-07-16 20:21:13 -0400

    add autopairs

---
+++ lua/peterf/plugins/nvim-autopairs.lua
@@ -0,0 +1,7 @@
+--[[
+Description:
+    A super powerful autopair plugin for Neovim that supports multiple characters.
+Link:
+    https://github.com/windwp/nvim-autopairs
+--]] --
+require("nvim-autopairs").setup {}
