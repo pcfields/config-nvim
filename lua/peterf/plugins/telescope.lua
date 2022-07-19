ac99112b8c1b6855ecdb40c38962aa5b6c1fe412
Peter Fields <pcfields@gmail.com>
2022-07-18 23:09:14 -0400

    exclude node_modules from telescope

--- lua/peterf/plugins/telescope.lua
+++ lua/peterf/plugins/telescope.lua
@@ -3,7 +3,12 @@ Description:
     telescope.nvim is a highly extendable fuzzy finder over lists.
 Link:         
     https://github.com/nvim-telescope/telescope.nvim
---]] local telescope = require('telescope')
+--]] --
+local telescope = require('telescope')
 
-telescope.setup {}
+telescope.setup {
+    defaults = {
+        file_ignore_patterns = {"node_modules"}
+    }
+}
 -- telescope.load_extension('fzf')
