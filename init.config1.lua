26075a82cea6e02eeb3f14d8f34104242a50da5e
Peter Fields <pcfields@gmail.com>
2022-12-27 00:51:26 +0100

    save config init files

---
+++ init.config1.lua
@@ -0,0 +1,15 @@
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
+require "config1.options"
+require "config1.keymaps"
+
+require "config1.plugins"
+require "config1.plugins.comment"
+require "config1.plugins.gitsigns"
+require "config1.plugins.indent-blankline"
+require "config1.plugins.lsp"
+require "config1.plugins.lualine"
+require "config1.plugins.nvim-cmp"
+require "config1.plugins.telescope"
+require "config1.plugins.treesitter"
+
--- lua/config1/plugins.lua
+++ lua/config1/plugins.lua
@@ -1,8 +1,10 @@
 -- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
+
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     is_bootstrap = true
+
     vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
     vim.cmd [[packadd packer.nvim]]
 end
@@ -64,6 +66,14 @@ require('packer').startup(function(use)
         cond = vim.fn.executable 'make' == 1
     }
 
+    use {
+        "akinsho/toggleterm.nvim",
+        tag = '*',
+        config = function()
+            require("toggleterm").setup()
+        end
+    }
+
     -- Add custom plugins to packer from /nvim/lua/custom/plugins.lua
     local has_plugins, plugins = pcall(require, 'custom.plugins')
     if has_plugins then
@@ -92,6 +102,7 @@ end
 local packer_group = vim.api.nvim_create_augroup('Packer', {
     clear = true
 })
+
 vim.api.nvim_create_autocmd('BufWritePost', {
     command = 'source <afile> | PackerCompile',
     group = packer_group,
