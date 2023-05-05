788bd9ae1492995628a9f37501fdae5b38e7ebea
Peter Fields <pcfields@gmail.com>
2023-05-05 18:30:23 +0200

    update colorscheme plugins

--- lua/pcfields/plugins/colorscheme.lua
+++ lua/pcfields/plugins/colorscheme.lua
@@ -1,20 +1,12 @@
-return { -- tokyonight
-{
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = {
-        style = "moon"
-    }
-}, -- catppuccin
-{
+return {{
     "catppuccin/nvim",
     lazy = true,
     name = "catppuccin"
-}, { -- Theme inspired by Atom
+}, {
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
         vim.cmd.colorscheme 'onedark'
     end
-}}
+}, {"sainnhe/gruvbox-material"}}
 
