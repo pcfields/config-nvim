531a488ac6852ace1b8245776cb22c7bd4614965
Peter Fields <pcfields@gmail.com>
2023-05-05 17:51:58 +0200

    move git signs to separate file

--- init.lua
+++ init.lua
@@ -55,19 +55,7 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
-  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+
 
   { -- Theme inspired by Atom
     'navarasu/onedark.nvim',
