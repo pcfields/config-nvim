2d42cc421c3c2fdcdaee781f1c446b02ee0f4194
Peter Fields <pcfields@gmail.com>
2023-05-05 20:55:01 +0200

    add keymaps for escape insert mode and toggle file explorer

--- lua/pcfields/config/keymaps.lua
+++ lua/pcfields/config/keymaps.lua
@@ -44,6 +44,9 @@ map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 -- quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
+-- Exit insert mode
+map("i", "<A-n>", "<esc>", { desc = "Exit insert mode" })
+
 -- -- better indenting
 map("v", "<", "<gv")
 map("v", ">", ">gv")
@@ -57,7 +60,10 @@ map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
 -- Display Lazy Plugin manager
 map("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
 
--- -- new file
+-- File explorer
+map("n", "<leader>fe", "<cmd>:Neotree toggle<cr>", { desc = "File explorer" })
+
+-- new file
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
 -- Display location list
@@ -102,51 +108,10 @@ map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 --   map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
 -- end
 
-
--- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
--- map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
--- map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
--- map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
--- map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
--- map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
--- map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-
--- -- Add undo break-points
--- map("i", ",", ",<c-g>u")
--- map("i", ".", ".<c-g>u")
--- map("i", ";", ";<c-g>u")
-
--- if not Util.has("trouble.nvim") then
---   map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
---   map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
--- end
-
--- stylua: ignore start
-
--- toggle options
--- map("n", "<leader>uf", require("lazyvim.plugins.lsp.format").toggle, { desc = "Toggle format on Save" })
--- map("n", "<leader>us", function() Util.toggle("spell") end, { desc = "Toggle Spelling" })
--- map("n", "<leader>uw", function() Util.toggle("wrap") end, { desc = "Toggle Word Wrap" })
--- map("n", "<leader>ul", function() Util.toggle("relativenumber", true) Util.toggle("number") end, { desc = "Toggle Line Numbers" })
--- map("n", "<leader>ud", Util.toggle_diagnostics, { desc = "Toggle Diagnostics" })
--- local conceallevel = vim.o.conceallevel > 0 and vim.o.conceallevel or 3
--- map("n", "<leader>uc", function() Util.toggle("conceallevel", false, {0, conceallevel}) end, { desc = "Toggle Conceal" })
-
--- lazygit
-map("n", "<leader>gg", function() Util.float_term({ "lazygit" }, { cwd = Util.get_root(), esc_esc = false }) end, { desc = "Lazygit (root dir)" })
-map("n", "<leader>gG", function() Util.float_term({ "lazygit" }, {esc_esc = false}) end, { desc = "Lazygit (cwd)" })
-
-
-
 -- highlights under cursor
 if vim.fn.has("nvim-0.9.0") == 1 then
   map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
 end
-
--- floating terminal
-map("n", "<leader>ft", function() Util.float_term(nil, { cwd = Util.get_root() }) end, { desc = "Terminal (root dir)" })
-map("n", "<leader>fT", function() Util.float_term() end, { desc = "Terminal (cwd)" })
-map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
 
 -- windows
 map("n", "<leader>ww", "<C-W>p", { desc = "Other window" })
