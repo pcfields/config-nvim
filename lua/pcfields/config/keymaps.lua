ed3172c5391e26e9a7fbd38a4cfa36a5fcfc323b
Peter Fields <pcfields@gmail.com>
2023-05-08 13:50:46 +0200

    update and organize keymaps

--- lua/pcfields/config/keymaps.lua
+++ lua/pcfields/config/keymaps.lua
@@ -38,8 +38,20 @@ map("n", "<leader>ur", "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>", {
 
 map({ "n", "x" }, "gw", "*N", { desc = "Search word under cursor" })
 
+-- Files --------------------------------------------------------------------------------------------
 -- save file
-map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+map({ "i", "v", "n", "s" }, "<leader>fs", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- Rename file
+map("n", "<leader>fr", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
+
+-- new file
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+
+-- File explorer
+map("n", "<leader>fe", "<cmd>:Neotree toggle<cr>", { desc = "File explorer" })
+
+--------------------------------------------------------------------------------------------
 
 -- quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
@@ -51,8 +63,6 @@ map("i", "<A-n>", "<esc>", { desc = "Exit insert mode" })
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
--- Rename file
-map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
 
 -- Code action
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
@@ -60,11 +70,7 @@ map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
 -- Display Lazy Plugin manager
 map("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
 
--- File explorer
-map("n", "<leader>fe", "<cmd>:Neotree toggle<cr>", { desc = "File explorer" })
 
--- new file
-map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
 -- Display location list
 map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
@@ -72,25 +78,28 @@ map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
 -- Display quickfix list
 map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
 
--- Move to window using the <ctrl> hjkl keys
+-- Move to window using the <ctrl> hjkl keys --------------------------------------------------------------------------------------------
 map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 
--- Resize window using <ctrl> arrow keys
+-- Resize window using <ctrl> arrow keys --------------------------------------------------------------------------------------------
 map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
--- Move Lines
+-- Move Lines --------------------------------------------------------------------------------------------
 map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
 map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+
 map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
 map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+
 map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+--------------------------------------------------------------------------------------------
 
 map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
