5af26d23f1d75ed729ce25937c8e77fa6e876797
Peter Fields <pcfields@gmail.com>
2023-05-12 23:46:26 +0200

    add bufferline

---
+++ lua/pcfields/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+-- https://github.com/akinsho/bufferline.nvim
+return {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+
+    end
+}
