cbb4cc366e555b65de56936263f681dd11b2b708
Peter Fields <peterf@glidertech.com>
2023-06-28 20:07:49 +0100

    change telescope keymaps and layout

--- lua/pcfields/plugins/telescope.lua
+++ lua/pcfields/plugins/telescope.lua
@@ -12,30 +12,39 @@ require('telescope').setup {
                 ['<C-u>'] = false,
                 ['<C-d>'] = false
             }
-        }
+        },
+        layout_strategy = 'horizontal',
+        layout_config = {
+            horizontal = {
+                height = 0.9,
+                preview_cutoff = 120,
+                prompt_position = "bottom",
+                width = 0.9
+            },
+        },
     }
 }
 
-
+local builtin = require('telescope.builtin');
 
 -- See `:help telescope.builtin`
-map('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-map('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+map('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+map('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 map('n', '<leader>si', function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require('telescope.builtin').current_buffer_fuzzy_find(
+    builtin.current_buffer_fuzzy_find(
         require('telescope.themes').get_dropdown {
             winblend = 10,
             previewer = false
         })
 end, { desc = 'Fuzzily [S]earch [I]n current buffer' })
 
-map('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-map('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-map('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-map('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-map('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-map('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+map('n', '<leader>sgf', builtin.git_files, { desc = '[S]earch [G]it [F]iles' })
+map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+map('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+map('n', '<leader>st', builtin.live_grep, { desc = '[S]earch by [t]ext using Grep' })
+map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 
 
