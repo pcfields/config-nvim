9cf1d1e066aa23e05c972d973d60dc7c43844d87
Peter Fields <peterf@glidertech.com>
2023-06-28 19:43:06 +0100

    change indent line color

--- lua/pcfields/plugins/indent-blankline.lua
+++ lua/pcfields/plugins/indent-blankline.lua
@@ -1,10 +1,10 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 -- Add indentation guides even on blank lines
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#222222 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#272727 gui=nocombine]]
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = false,
-    show_current_context_start = true,
+    show_current_context_start = false,
     char_highlight_list = {"IndentBlanklineIndent1"}
 }
