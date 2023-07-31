f22a176c42663b303236d86f7b817aabcf8115fc
Peter Fields <peterf@glidertech.com>
2023-07-31 17:42:06 +0100

    update lualine config

--- lua/pcfields/plugins/lualine.lua
+++ lua/pcfields/plugins/lualine.lua
@@ -5,25 +5,25 @@ require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'codedark', -- auto | palenight | base16 16color ayu_dark  ayu_mirage codedark
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
+        component_separators = '',
+        section_separators = { left = '', right = ''}, 
         disabled_filetypes = {
-        statusline = {},
-        winbar = {},
+            statusline = {},
+            winbar = {},
         },
         ignore_focus = {},
         always_divide_middle = true,
         globalstatus = false,
         refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
         }
     },
     sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_b = {'filename'},
+        lualine_c = {'branch', 'diff', 'diagnostics'},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
