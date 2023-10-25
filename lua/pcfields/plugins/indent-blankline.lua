eef57f396777a3b9faf35afc724062222b7b6bc4
Peter Fields <pcfields@pm.me>
2023-10-25 21:24:53 +0200

    update indent-blankline config

--- lua/pcfields/plugins/indent-blankline.lua
+++ lua/pcfields/plugins/indent-blankline.lua
@@ -1,10 +1,6 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
--- Add indentation guides even on blank lines
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#272727 gui=nocombine]]
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = false,
-    show_current_context_start = false,
-    char_highlight_list = {"IndentBlanklineIndent1"}
-}
+-- Add indentation guides even on blank lines
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#272727 gui=nocombine]]
+
+require("ibl").setup {}
