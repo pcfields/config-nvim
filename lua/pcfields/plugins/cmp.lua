58c25df492097c5858e777e47a147ee3360ebad8
Peter Fields <pcfields@pm.me>
2023-11-18 17:54:01 +0100

    update nvim-cmp config

--- lua/pcfields/plugins/cmp.lua
+++ lua/pcfields/plugins/cmp.lua
@@ -1,49 +1,56 @@
 -- https://github.com/hrsh7th/nvim-cmp
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
-luasnip.config.setup {}
+luasnip.config.setup({})
 
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert {
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        -- ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        -- ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' })
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-    }
-}
+-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+	completion = {
+		completeopt = "menu,menuone,preview,noselect",
+	},
+	snippet = { -- configure how nvim-cmp interacts with snippet engine
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+		["<C-u>"] = cmp.mapping.scroll_docs(-4), -- scoll up docs window
+		["<C-d>"] = cmp.mapping.scroll_docs(4), -- scroll down docs window
+		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = { -- sources for autocompletion
+		{ name = "nvim_lsp" }, -- lsp
+		{ name = "luasnip" }, -- snippets
+		{ name = "buffer" }, -- text within buffer
+		{ name = "path" }, -- file system paths
+	},
+})
