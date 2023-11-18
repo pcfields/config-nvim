10213e6459ce02ace1d51ca2f33e5f69d9320c1a
Peter Fields <pcfields@pm.me>
2023-11-18 17:52:30 +0100

    move auto-commands to config folder

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
-require "pcfields.config.options"
-require "pcfields.config.keymaps"
-require "pcfields.auto-commands"
-require "pcfields.config.plugin-manager"
-require "pcfields.plugins"
-
+-- Load configuration
+require("pcfields.config.options")
+require("pcfields.config.keymaps")
+require("pcfields.config.auto-commands")
+require("pcfields.config.plugin-manager")
+-- Load plugins
+require("pcfields.plugins")
