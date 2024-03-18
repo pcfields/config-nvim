b7dd6eca5caff08a001c37279acc4ed138eebeed
Peter Fields <peterf@glidertech.com>
2024-03-18 15:03:27 +0000

    update plugin files

--- init.lua
+++ init.lua
@@ -3,5 +3,4 @@ require 'pcfields.config.options'
 require 'pcfields.config.keymaps'
 require 'pcfields.config.auto-commands'
 require 'pcfields.config.plugin-manager'
--- Load plugins
-require 'pcfields.plugins.active-plugins'
+require 'pcfields.config.active-plugins'-- Load plugins
