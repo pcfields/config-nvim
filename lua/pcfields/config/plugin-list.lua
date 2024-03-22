7281d48aa5ea263e399b9fec46cc051563d2847e
Peter <pcfields@gmail.com>
2024-03-22 17:00:05 +0100

    add and configure noice plugin

--- lua/pcfields/config/plugin-list.lua
+++ lua/pcfields/config/plugin-list.lua
@@ -8,7 +8,7 @@ end
 
 local function load_config(package)
     return function()
-        require('plugins.' .. package)
+        require('pcfields/plugins.' .. package)
     end
 end
 
@@ -16,8 +16,8 @@ local plugins = {
     -- Colorschemes ----------------------------------------------------------------------------------------
     {
         'navarasu/onedark.nvim', -- https://github.com/navarasu/onedark.nvim
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        lazy = false,            -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000,         -- make sure to load this before all the other start plugins
     },
     {
         'Mofiqul/vscode.nvim', -- https://github.com/Mofiqul/vscode.nvim
@@ -29,21 +29,21 @@ local plugins = {
     -- Git related plugins
     'tpope/vim-fugitive',
     'tpope/vim-rhubarb',
+    'lewis6991/gitsigns.nvim',
+    'kdheepak/lazygit.nvim',
     -------------------------------------------------------------------------------------------------------
     -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
     'windwp/nvim-autopairs',
     -------------------------------------------------------------------------------------------------------
     'm4xshen/autoclose.nvim', -- https://github.com/m4xshen/autoclose.nvim
-    'numToStr/Comment.nvim', -- https://github.com/numToStr/Comment.nvim
-    'lewis6991/gitsigns.nvim',
+    'numToStr/Comment.nvim',  -- https://github.com/numToStr/Comment.nvim
     'lukas-reineke/indent-blankline.nvim',
     'nvim-lualine/lualine.nvim',
     'jose-elias-alvarez/null-ls.nvim',
     'folke/twilight.nvim',
     'folke/which-key.nvim',
     'nvim-tree/nvim-web-devicons',
-    'kdheepak/lazygit.nvim',
     'nvim-lua/plenary.nvim',
     {
         'folke/trouble.nvim', -- https://github.com/folke/trouble.nvim
@@ -92,8 +92,8 @@ local plugins = {
             'hrsh7th/cmp-nvim-lsp',
             'saadparwaiz1/cmp_luasnip', -- source for autocompletion
             'L3MON4D3/LuaSnip',
-            'hrsh7th/cmp-buffer', -- source for text in buffer
-            'hrsh7th/cmp-path', -- source for file system paths
+            'hrsh7th/cmp-buffer',       -- source for text in buffer
+            'hrsh7th/cmp-path',         -- source for file system paths
             'hrsh7th/cmp-cmdline',
             'hrsh7th/nvim-cmp',
             'rafamadriz/friendly-snippets', -- useful snippets
@@ -111,14 +111,14 @@ local plugins = {
                 'j-hui/fidget.nvim',
                 tag = 'legacy',
                 opts = {},
-            }, -- Useful status updates for LSP
+            },                       -- Useful status updates for LSP
             { 'folke/neodev.nvim' }, -- Additional lua configuration, makes nvim stuff amazing!
         },
     },
     {
         'mfussenegger/nvim-dap',
-        dependencies = { -- Creates a beautiful debugger UI
-            'rcarriga/nvim-dap-ui', -- Installs the debug adapters for you
+        dependencies = {                    -- Creates a beautiful debugger UI
+            'rcarriga/nvim-dap-ui',         -- Installs the debug adapters for you
             'williamboman/mason.nvim',
             'jay-babu/mason-nvim-dap.nvim', -- Add your own debuggers here
             -- 'leoluz/nvim-dap-go',
@@ -128,12 +128,26 @@ local plugins = {
         'codota/tabnine-nvim',
         build = tabnine_build_path(),
     },
-    'David-Kunz/jester', -- https://github.com/David-Kunz/jester
+    { 'David-Kunz/jester' }, -- https://github.com/David-Kunz/jester
     {
         'stevearc/dressing.nvim',
         opts = {},
     },
     { 'kevinhwang91/nvim-ufo', dependencies = 'kevinhwang91/promise-async' },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        config = load_config('noice'),
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    }
 }
 
 return {
