58dd8ad4ac70cbd7ea29cf5907982ef1f145e9ae
Peter Fields <peterf@glidertech.com>
2024-04-08 14:28:30 +0100

    add gitsigns keympas

--- lua/pcfields/plugins/gitsigns.lua
+++ lua/pcfields/plugins/gitsigns.lua
@@ -1,8 +1,12 @@
 -- https://github.com/lewis6991/gitsigns.nvim
 -- Adds git releated signs to the gutter, as well as utilities for managing changes
+-- See `:help gitsigns.txt`
 
-require('gitsigns').setup {
-    -- See `:help gitsigns.txt`
+local map = require('pcfields.utils').map
+
+local gitsigns = require('gitsigns')
+
+gitsigns.setup {
     signs = {
         add = {
             text = '+',
@@ -20,4 +24,10 @@ require('gitsigns').setup {
             text = '~',
         },
     },
+    on_attach = function()
+        -- Actions
+        map('n', '<leader>gd', gitsigns.diffthis)
+        map('n', '<leader>gb', function() gitsigns.blame_line { full = true } end)
+        map('n', '<leader>gbl', gitsigns.toggle_current_line_blame)
+    end
 }
