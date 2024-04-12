ae73923242f5952ac505d6e0976f189dbc234199
Peter Fields <peterf@glidertech.com>
2024-04-12 10:00:10 +0100

    update bufferline config

--- lua/pcfields/plugins/bufferline.lua
+++ lua/pcfields/plugins/bufferline.lua
@@ -5,10 +5,6 @@ require('bufferline').setup {
         mode = 'buffers',         -- set to "tabs" to only show tabpages instead
         diagnostics = 'nvim_lsp', -- false | "nvim_lsp" | "coc",
         numbers = 'ordinal',      --"none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-        indicator = {
-            icon = '▎',         -- this should be omitted if indicator style is not 'icon'
-            style = 'icon',       --  'icon' | 'underline' | 'none',
-        },
         diagnostics_indicator = function(count, level, diagnostics_dict, context)
             local icon = level:match 'error' and ' ' or ' '
             return ' ' .. icon .. count
@@ -18,14 +14,15 @@ require('bufferline').setup {
                 filetype = 'neo-tree',
                 text = 'File Explorer',
                 text_align = 'left',
-                highlight = "Directory"
+                highlight = "Directory",
+                separator = true -- use a "true" to enable the default, or set your own character
             },
         },
-        show_buffer_icons = false, -- disable filetype icons for buffers
+        show_buffer_icons = true, -- disable filetype icons for buffers
         show_buffer_close_icons = true,
         show_close_icon = true,
         show_tab_indicators = true,
-        separator_style = 'slant',
-        always_show_bufferline = false
+        separator_style = 'thick', -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
+        always_show_bufferline = true
     },
 }
