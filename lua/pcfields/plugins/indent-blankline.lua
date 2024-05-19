99d6b2d42c7f0b755aeab729faffd678715a067a
Peter Fields <peterf@glidertech.com>
2024-05-19 18:03:30 +0100

    remove indent blankline

--- lua/pcfields/plugins/indent-blankline.lua
+++
@@ -1,6 +0,0 @@
--- https://github.com/lukas-reineke/indent-blankline.nvim
-
--- Add indentation guides even on blank lines
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#272727 gui=nocombine]]
-
-require('ibl').setup {}
