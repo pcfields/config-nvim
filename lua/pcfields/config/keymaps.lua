301a154605757320d6b7f72e4ac010ad5a667cd0
Peter <pcfields@gmail.com>
2024-06-01 20:06:51 +0200

    update keymaps

--- lua/pcfields/config/keymaps.lua
+++ lua/pcfields/config/keymaps.lua
@@ -18,8 +18,7 @@ map({ 'i', 'n', 'v' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear 
 map({ 'i', 'n', 'v' }, '<C-e>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
 -- Clear search, diff update and redraw
-map('n', '<leader>ur', '<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>',
-  { desc = 'Redraw / clear hlsearch / diff update' })
+map('n', '<leader>ur', '<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>', { desc = 'Redraw / clear hlsearch / diff update' })
 
 -- Search word under cursor
 map({ 'n', 'x' }, 'gw', '*N', { desc = 'Search word under cursor' })
@@ -29,7 +28,7 @@ map({ 'n', 'x' }, 'gw', '*N', { desc = 'Search word under cursor' })
 --------------------------------------------------------------------------------------------
 
 -- Save file
-map({ 'n', 'v', 's' }, '<leader>fs', '<cmd>w<cr><esc>', { desc = 'Save file' })
+map({ 'n', 'v', 's' }, '<leader>aa', '<cmd>w<cr><esc>', { desc = 'Save file' })
 
 -- New file
 map('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'File new ' })
@@ -38,16 +37,13 @@ map('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'File new ' })
 map('n', '<leader>ff', '<cmd>Format<cr>', { desc = 'File format' })
 
 -- File explorer
-map('n', '<leader>fe', '<cmd>:Neotree toggle<cr>', { desc = 'File explorer' })
+map('n', '<leader>oe', '<cmd>:Neotree toggle<cr>', { desc = 'File explorer' })
 
 -- File Explorer git status
-map('n', '<leader>fg', '<cmd>:Neotree git_status<cr>', { desc = 'File git status' })
-
--- Close buffer
-map('n', '<leader>fx', '<cmd>:bd<cr>', { desc = 'File Close buffer' })
+map('n', '<leader>eg', '<cmd>:Neotree git_status<cr>', { desc = 'File git status' })
 
 -- Select all text in buffer
-map("n", "<C-a>", "gg<S-v>G", { desc = "Select all text in buffer" })
+map('n', '<C-a>', 'gg<S-v>G', { desc = 'Select all text in buffer' })
 
 -- Next buffer
 map('n', '<A-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
@@ -55,8 +51,12 @@ map('n', '<A-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
 -- Previous buffer
 map('n', '<A-h>', '<cmd>bprevious<cr>', { desc = 'Previous buffer' })
 
+-- Close buffer
+map('n', '<leader>xx', '<cmd>:bd<cr>', { desc = 'File Close buffer' })
+
 -- Bufferline buffers
-map('n', '<leader>sb', '<cmd>:BufferLinePick<cr>', { desc = 'Select a buffer with letter' })
+map('n', '<leader>bb', '<cmd>:BufferLinePick<cr>', { desc = 'Select a buffer with letter' })
+
 map('n', '<leader>1', '<cmd>BufferLineGoToBuffer 1<cr>', { desc = 'Go to buffer 1' })
 map('n', '<leader>2', '<cmd>BufferLineGoToBuffer 2<cr>', { desc = 'Go to buffer 2' })
 map('n', '<leader>3', '<cmd>BufferLineGoToBuffer 3<cr>', { desc = 'Go to buffer 3' })
@@ -66,16 +66,17 @@ map('n', '<leader>6', '<cmd>BufferLineGoToBuffer 6<cr>', { desc = 'Go to buffer 
 map('n', '<leader>7', '<cmd>BufferLineGoToBuffer 7<cr>', { desc = 'Go to buffer 7' })
 map('n', '<leader>8', '<cmd>BufferLineGoToBuffer 8<cr>', { desc = 'Go to buffer 8' })
 map('n', '<leader>9', '<cmd>BufferLineGoToBuffer 9<cr>', { desc = 'Go to buffer 9' })
+
 map('n', '<leader>0', '<cmd>BufferLineGoToBuffer -1<cr>', { desc = 'Go to last buffer' })
 
 -- Close all buffers to the RIGHT
-map('n', '<leader>bl', '<cmd>BufferLineCloseRight<cr>', { desc = 'Close all buffers to the RIGHT' })
+map('n', '<leader>xl', '<cmd>BufferLineCloseRight<cr>', { desc = 'Close all buffers to the RIGHT' })
 
 -- Close all buffer to the LEFT
-map('n', '<leader>bh', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Close all buffer to the LEFT' })
+map('n', '<leader>xh', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Close all buffer to the LEFT' })
 
 -- Close all other buffers
-map('n', '<leader>bo', '<cmd>BufferLineCloseOthers<cr>', { desc = 'Close all other buffers' })
+map('n', '<leader>xo', '<cmd>BufferLineCloseOthers<cr>', { desc = 'Close all other buffers' })
 
 -- windows
 map('n', '<leader>ww', '<C-w>p', { desc = 'Other window' })
@@ -106,10 +107,10 @@ map('n', '<C-Right>', '<cmd>vertical resize +4<cr>', { desc = 'Decrease window w
 
 --------------------------------------------------------------------------------------------
 -- Quit All
-map('n', '<leader>qa', '<cmd>qa<cr>', { desc = 'Quit all' })
+map('n', '<leader>xa', '<cmd>qa<cr>', { desc = 'Quit all' })
 
 -- Quit
-map('n', '<leader>qx', '<cmd>q<cr>', { desc = 'Quit' })
+map('n', '<leader>xc', '<cmd>q<cr>', { desc = 'Quit' })
 
 -- Exit insert mode
 map('i', 'jj', '<esc>', { desc = 'Exit insert mode' })
@@ -136,19 +137,19 @@ map('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move up' })
 
 -- highlights under cursor
 if vim.fn.has 'nvim-0.9.0' == 1 then
-  map('n', '<leader>ui', vim.show_pos, { desc = 'Inspect Pos' })
+    map('n', '<leader>ui', vim.show_pos, { desc = 'Inspect Pos' })
 end
 
 -- terminal
 function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
@@ -157,8 +158,6 @@ vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
 --------------------------------------------------------------------------------------------
 -- Open things -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------
--- Open Diagnostics window
-map('n', '<leader>od', '<cmd>TroubleToggle<cr>', { desc = 'Open diagnostics' })
 -- Open Lazygit
 map('n', '<leader>og', '<cmd>:LazyGit<cr>', { desc = 'Open LazyGit' })
 -- Display quickfix list
@@ -167,6 +166,8 @@ map('n', '<leader>oq', '<cmd>copen<cr>', { desc = 'Quickfix List' })
 map('n', '<leader>ot', '<cmd>:ToggleTerm<cr>', { desc = 'Open ToggleTerm' })
 
 -- Diagnostic keymaps
+-- Open Diagnostics window
+map('n', '<leader>od', '<cmd>TroubleToggle<cr>', { desc = 'Open diagnostics' })
 map('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic message' })
 map('n', '<leader>dn', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic message' })
 map('n', '<leader>dm', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
