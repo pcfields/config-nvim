991580bc44c808cc81747fde66e3ba3849c00354
Peter Fields <peterf@glidertech.com>
2024-07-16 16:52:01 +0100

    update tabnine shortcuts

--- lua/pcfields/plugins/tabnine.lua
+++ lua/pcfields/plugins/tabnine.lua
@@ -1,9 +1,26 @@
 require('tabnine').setup {
     disable_auto_comment = true,
-    accept_keymap = '<C-;>',
-    dismiss_keymap = '<C-,>',
+    accept_keymap = '<C-9>',
+    dismiss_keymap = '<C-]>',
     debounce_ms = 800,
     suggestion_color = { gui = '#808080', cterm = 244 },
-    exclude_filetypes = { 'TelescopePrompt' },
+    exclude_filetypes = { 'TelescopePrompt', "NvimTree" },
     log_file_path = nil, -- absolute path to Tabnine log file
 }
+
+
+-- You can set `accept_keymap` and `dismiss_keymap` to `false` to disable them
+-- then you can create mappings using `require('tabnine.keymaps')`
+
+--- Example integration with Tabnine and LuaSnip
+--- falling back to inserting tab if neither has a completion
+
+-- vim.keymap.set("i", "<tab>", function()
+--   if require("tabnine.keymaps").has_suggestion() then
+--     return require("tabnine.keymaps").accept_suggestion()
+--   elseif require("luasnip").jumpable(1) then
+--     return require("luasnip").jump(1)
+--   else
+--     return "<tab>"
+--   end
+-- end, { expr = true })
