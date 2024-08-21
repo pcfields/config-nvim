cdafa614bc073ad3f605e4208f012b4d44b8052a
Peter Fields <peterf@glidertech.com>
2024-08-21 13:50:11 +0100

    include hidden files in telescope file

--- lua/pcfields/plugins/telescope.lua
+++ lua/pcfields/plugins/telescope.lua
@@ -4,82 +4,61 @@
 -- Fuzzy Finder (files, lsp, etc)
 
 local map = require('pcfields.utils').map
+local telescope_builtin = require 'telescope.builtin'
+local telescope = require 'telescope'
 
-require('telescope').setup {
+telescope.setup {
     defaults = {
-        path_display = { shorten = { len = 1 } },
+        -- path_display = { shorten = { len = 3 } },
+        path_display = { 'truncate' },
         mappings = {
             i = {
                 ['<C-u>'] = false,
                 ['<C-d>'] = false,
             },
         },
-        layout_strategy = 'horizontal',
-        layout_config = {
-            bottom_pane = {
-                height = 25,
-                preview_cutoff = 120,
-                prompt_position = "top"
-            },
-            center = {
-                height = 0.5,
-                preview_cutoff = 40,
-                prompt_position = "top",
-                width = 0.5
-            },
-            cursor = {
-                height = 0.9,
-                preview_cutoff = 40,
-                width = 0.8
-            },
-            horizontal = {
-                height = 0.9,
-                preview_cutoff = 120,
-                prompt_position = "bottom",
-                width = 0.8
-            },
-            vertical = {
-                height = 0.9,
-                preview_cutoff = 40,
-                prompt_position = "bottom",
-                width = 0.8
-            }
-        },
     },
 }
 
 -- See `:help telescope.builtin`
-local builtin = require 'telescope.builtin'
 
 local telescope_themes_dropdown = function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
-    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    telescope_builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
         winblend = 10,
         previewer = false,
     })
 end
 
+local search_files = function()
+    if vim.fn.filereadable './.git/HEAD' then
+        telescope_builtin.git_files { use_git_root = false, show_untracked = true }
+    else
+        telescope_builtin.search_files()
+    end
+end
+
 -- Files
-map('n', '<leader><space>', builtin.buffers, { desc = 'Find existing buffers' })
-map('n', '<leader>sr', builtin.oldfiles, { desc = 'Search recently opened files' })
-map('n', '<leader>sf', builtin.find_files, { desc = 'Search Files' })
+map('n', '<leader><space>', telescope_builtin.buffers, { desc = 'Find existing buffers' })
+map('n', '<leader>sr', telescope_builtin.oldfiles, { desc = 'Search recently opened files' })
+map('n', '<leader>sf', search_files, { desc = 'Search Files' })
 map('n', '<leader>si', telescope_themes_dropdown, { desc = 'Fuzzily Search In current buffer' })
 -- Jumplist
-map('n', '<leader>sj', builtin.jumplist, { desc = 'Search Jump List' })
+map('n', '<leader>sj', telescope_builtin.jumplist, { desc = 'Search Jump List' })
 -- Text
-map('n', '<leader>sw', builtin.grep_string, { desc = 'Search current Word' })
-map('n', '<leader>se', builtin.live_grep, { desc = 'Search everywhere by text using Grep' })
-map('n', '<leader>sh', builtin.help_tags, { desc = 'Search Help' })
+map('n', '<leader>sw', telescope_builtin.grep_string, { desc = 'Search current Word' })
+map('n', '<leader>se', telescope_builtin.live_grep, { desc = 'Search everywhere by text using Grep' })
+map('n', '<leader>sh', telescope_builtin.help_tags, { desc = 'Search Help' })
 -- Diagnostics
-map('n', '<leader>sd', builtin.diagnostics, { desc = 'Search Diagnostics' })
+map('n', '<leader>sd', telescope_builtin.diagnostics, { desc = 'Search Diagnostics' })
 -- Git
-map('n', '<leader>gf', builtin.git_files, { desc = 'Searh Git Files' })
-map('n', '<leader>gs', builtin.git_status, { desc = 'Search Git Satus' })
-map('n', '<leader>gb', builtin.git_branches, { desc = 'Search Git Branches' })
+map('n', '<leader>sg', telescope_builtin.git_files, { desc = 'Searh Git Files' })
+map('n', '<leader>gs', telescope_builtin.git_status, { desc = 'Search Git Satus' })
+map('n', '<leader>gb', telescope_builtin.git_branches, { desc = 'Search Git Branches' })
 
 -- Fuzzy Finder Algorithm which requires local dependencies to be built.
 -- Only load if `make` is available. Make sure you have the system
 -- requirements installed.
 --
 -- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
+pcall(telescope.load_extension, 'fzf')
