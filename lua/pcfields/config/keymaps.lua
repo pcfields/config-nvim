903a6acf92c98c6ec0adc55b60ed5be4676d294f
Peter <pcfields@gmail.com>
2024-09-29 00:59:18 +0200

    add descriptions to keymaps and improve labels

--- lua/pcfields/config/keymaps.lua
+++ lua/pcfields/config/keymaps.lua
@@ -9,143 +9,127 @@ map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Remap for dealing with word wrap
-map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 -- Clear search with <esc>
 map({ 'i', 'n', 'v' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 map({ 'i', 'n', 'v' }, '<C-e>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
 -- Clear search, diff update and redraw
-map('n', '<leader>ur', '<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>', { desc = 'Redraw / clear hlsearch / diff update' })
+map({ 'n' }, '<leader>ur', '<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>', { desc = 'Redraw / clear hlsearch / diff update' })
 
 -- Search word under cursor
 map({ 'n', 'x' }, 'gw', '*N', { desc = 'Search word under cursor' })
 
-map('x', 'p', [["_dP]]) -- Fix copy and paste, keep the copied text instead of keeping the text pasted over
+map({ 'x' }, 'p', [["_dP]]) -- Fix copy and paste, keep the copied text instead of keeping the text pasted over
 
--- Delete to end of line
-map('n', '<leader>de', 'd$')
-
-map('n', 'n', 'nzz')
-map('n', 'N', 'Nzz')
-map('n', '*', '*zz')
-map('n', '#', '#zz')
-map('n', 'g*', 'g*zz')
-map('n', 'g#', 'g#zz')
 --------------------------------------------------------------------------------------------
--- Files + Buffers -----------------------------------------------------------------------------------
+-- Files  -----------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------
 
 map({ 'n' }, '<leader>ss', '/', { desc = 'Search', noremap = true, silent = false })
--- Save file
 map({ 'n', 'v', 's' }, '<leader>fs', '<cmd>w<cr><esc>', { desc = 'Save file' })
-
--- New file
-map('n', '<leader>nf', '<cmd>enew<cr>', { desc = 'File new ' })
-
--- File explorer
-map('n', '<leader>fw', '<cmd>Neotree toggle reveal=true<cr>', { desc = 'File explorer' })
-
--- File Explorer git status
-map('n', '<leader>fg', '<cmd>Neotree git_status<cr>', { desc = 'File git status' })
-
--- Select all text in buffer
-map('n', '<leader>aa', 'gg<S-v>G', { desc = 'Select all text in buffer' })
-
--- Next buffer
-map('n', '<A-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
-
--- Previous buffer
-map('n', '<A-h>', '<cmd>bprevious<cr>', { desc = 'Previous buffer' })
-
--- Close buffer
-map('n', '<leader>xx', '<cmd>:bd<cr>', { desc = 'File Close buffer' })
-
--- Bufferline buffers
-map('n', '<leader>bb', '<cmd>:BufferLinePick<cr>', { desc = 'Select a buffer with letter' })
-
-map('n', '<leader>1', '<cmd>BufferLineGoToBuffer 1<cr>', { desc = 'Go to buffer 1' })
-map('n', '<leader>2', '<cmd>BufferLineGoToBuffer 2<cr>', { desc = 'Go to buffer 2' })
-map('n', '<leader>3', '<cmd>BufferLineGoToBuffer 3<cr>', { desc = 'Go to buffer 3' })
-map('n', '<leader>4', '<cmd>BufferLineGoToBuffer 4<cr>', { desc = 'Go to buffer 4' })
-map('n', '<leader>5', '<cmd>BufferLineGoToBuffer 5<cr>', { desc = 'Go to buffer 5' })
-map('n', '<leader>6', '<cmd>BufferLineGoToBuffer 6<cr>', { desc = 'Go to buffer 6' })
-map('n', '<leader>7', '<cmd>BufferLineGoToBuffer 7<cr>', { desc = 'Go to buffer 7' })
-map('n', '<leader>8', '<cmd>BufferLineGoToBuffer 8<cr>', { desc = 'Go to buffer 8' })
-map('n', '<leader>9', '<cmd>BufferLineGoToBuffer 9<cr>', { desc = 'Go to buffer 9' })
-
-map('n', '<leader>0', '<cmd>BufferLineGoToBuffer -1<cr>', { desc = 'Go to last buffer' })
-
--- Close all buffers to the RIGHT
-map('n', '<leader>bl', '<cmd>BufferLineCloseRight<cr>', { desc = 'Close all buffers to the RIGHT' })
-
--- Close all buffer to the LEFT
-map('n', '<leader>bh', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Close all buffer to the LEFT' })
-
--- Close all other buffers
-map('n', '<leader>bo', '<cmd>BufferLineCloseOthers<cr>', { desc = 'Close all other buffers' })
-
--- windows
-map('n', '<leader>ww', '<C-w>p', { desc = 'Other window' })
-map('n', '<leader>wx', '<C-w>c', { desc = 'Delete window' })
-map('n', '<leader>wn', '<C-w>n', { desc = 'Create new window' })
-
--- Move to window using the <ctrl> hjkl keys
-map('n', '<leader>wh', '<C-w>h', { desc = 'Go to left window' })
-map('n', '<leader>wj', '<C-w>j', { desc = 'Go to lower window' })
-map('n', '<leader>wk', '<C-w>k', { desc = 'Go to upper window' })
-map('n', '<leader>wl', '<C-w>l', { desc = 'Go to right window' })
-
--- Split windows
-map('n', '<leader>wb', '<C-w>s', { desc = 'Split window below' })
-map('n', '<leader>wr', '<C-w>v', { desc = 'Split window right' })
-
--- Resize windows 50/50
-map('n', '<leader>w2', '<C-w>=', { desc = 'Resize windows to be 50|50' })
-
--- Resize window using <ctrl> arrow keys --------------------------------------------------------------------------------------------
--- Height
-map('n', '<C-Up>', '<cmd>resize +4<cr>', { desc = 'Increase window height' })
-map('n', '<C-Down>', '<cmd>resize -4<cr>', { desc = 'Decrease window height' })
-
--- Vertical Width small adjustment
-map('n', '<C-Left>', '<cmd>vertical resize +4<cr>', { desc = 'Increase window width' })
-map('n', '<C-Right>', '<cmd>vertical resize -4<cr>', { desc = 'Decrease window width' })
+map({ 'n' }, '<leader>nf', '<cmd>enew<cr>', { desc = 'File new ' })
 
 --------------------------------------------------------------------------------------------
--- Quit All
-map('n', '<leader>xa', '<cmd>qa<cr>', { desc = 'Quit all' })
+-- File explorer -----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
 
--- Quit
-map('n', '<leader>xc', '<cmd>q<cr>', { desc = 'Quit' })
+map({ 'n' }, '<leader>fw', '<cmd>Neotree toggle reveal=true<cr>', { desc = 'File explorer' })
+map({ 'n' }, '<leader>fg', '<cmd>Neotree git_status<cr>', { desc = 'File git status' })
+map({ 'n' }, '<leader>aa', 'gg<S-v>G', { desc = 'Select all text in buffer' })
 
--- Exit insert mode
+--------------------------------------------------------------------------------------------
+-- Buffers ----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+
+map({ 'n' }, '<leader>xx', '<cmd>:bd<cr>', { desc = 'Close buffer' })
+map({ 'n' }, '<A-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
+map({ 'n' }, '<A-h>', '<cmd>bprevious<cr>', { desc = 'Previous buffer' })
+map({ 'n' }, '<leader>bb', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
+map({ 'n' }, '<leader>1', '<cmd>BufferLineGoToBuffer 1<cr>', { desc = 'Go to buffer 1' })
+map({ 'n' }, '<leader>2', '<cmd>BufferLineGoToBuffer 2<cr>', { desc = 'Go to buffer 2' })
+map({ 'n' }, '<leader>3', '<cmd>BufferLineGoToBuffer 3<cr>', { desc = 'Go to buffer 3' })
+map({ 'n' }, '<leader>4', '<cmd>BufferLineGoToBuffer 4<cr>', { desc = 'Go to buffer 4' })
+map({ 'n' }, '<leader>5', '<cmd>BufferLineGoToBuffer 5<cr>', { desc = 'Go to buffer 5' })
+map({ 'n' }, '<leader>6', '<cmd>BufferLineGoToBuffer 6<cr>', { desc = 'Go to buffer 6' })
+map({ 'n' }, '<leader>7', '<cmd>BufferLineGoToBuffer 7<cr>', { desc = 'Go to buffer 7' })
+map({ 'n' }, '<leader>8', '<cmd>BufferLineGoToBuffer 8<cr>', { desc = 'Go to buffer 8' })
+map({ 'n' }, '<leader>9', '<cmd>BufferLineGoToBuffer 9<cr>', { desc = 'Go to buffer 9' })
+map({ 'n' }, '<leader>0', '<cmd>BufferLineGoToBuffer -1<cr>', { desc = 'Go to last buffer' })
+map({ 'n' }, '<leader>bl', '<cmd>BufferLineCloseRight<cr>', { desc = 'Close all buffers to the RIGHT' })
+map({ 'n' }, '<leader>bh', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Close all buffer to the LEFT' })
+map({ 'n' }, '<leader>bo', '<cmd>BufferLineCloseOthers<cr>', { desc = 'Close all other buffers' })
+-- map({ 'n' }, '<leader>bb', '<cmd>:BufferLinePick<cr>', { desc = 'Select a buffer with letter' })
+
+--------------------------------------------------------------------------------------------
+-- Windows ---------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+
+map({ 'n' }, '<leader>ww', '<C-w>p', { desc = 'Other window' })
+map({ 'n' }, '<leader>wx', '<C-w>c', { desc = 'Delete window' })
+map({ 'n' }, '<leader>wn', '<C-w>n', { desc = 'Create new window' })
+
+-- Move to window using the <ctrl> hjkl keys
+map({ 'n' }, '<leader>wh', '<C-w>h', { desc = 'Go to left window' })
+map({ 'n' }, '<leader>wj', '<C-w>j', { desc = 'Go to lower window' })
+map({ 'n' }, '<leader>wk', '<C-w>k', { desc = 'Go to upper window' })
+map({ 'n' }, '<leader>wl', '<C-w>l', { desc = 'Go to right window' })
+
+-- Split windows
+map({ 'n' }, '<leader>wb', '<C-w>s', { desc = 'Split window below' })
+map({ 'n' }, '<leader>wr', '<C-w>v', { desc = 'Split window right' })
+
+-- Resize windows 50/50
+map({ 'n' }, '<leader>w2', '<C-w>=', { desc = 'Resize windows to be 50|50' })
+
+--------------------------------------------------------------------------------------------
+-- Resize ---------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+map({ 'n' }, '<C-Up>', '<cmd>resize +4<cr>', { desc = 'Increase window height' })
+map({ 'n' }, '<C-Down>', '<cmd>resize -4<cr>', { desc = 'Decrease window height' })
+map({ 'n' }, '<C-Left>', '<cmd>vertical resize +4<cr>', { desc = 'Increase window width' })
+map({ 'n' }, '<C-Right>', '<cmd>vertical resize -4<cr>', { desc = 'Decrease window width' })
+
+--------------------------------------------------------------------------------------------
+map({ 'n' }, '<leader>xa', '<cmd>qa<cr>', { desc = 'Quit all, Close Neovim' })
+map({ 'n' }, '<leader>xc', '<cmd>q<cr>', { desc = 'Quit' })
 map('i', 'jk', '<esc>', { desc = 'Exit insert mode' })
 
--- Line movement - left, right
+--------------------------------------------------------------------------------------------
+-- Line movement ---------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+
+map({ 'n' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Move up one line and manage word wrap', expr = true, silent = true })
+map({ 'n' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Move down one line and manage word wrap', expr = true, silent = true })
+map({ 'n' }, 'n', 'nzz', { desc = 'Go to next and center cursor in middle of screen' })
+map({ 'n' }, 'N', 'Nzz', { desc = 'Go to previous and center cursor in middle of screen' })
+map({ 'n' }, '*', '*zz', { desc = 'Search forward for the word under the cursor and center cursor in middle of screen' })
+map({ 'n' }, '#', '#zz', { desc = 'Search backward and center cursor in middle of screen' })
+map({ 'n' }, 'g*', 'g*zz', { desc = 'Search forward for the word under the cursor and center cursor in middle of screen' })
+map({ 'n' }, 'g#', 'g#zz', { desc = 'Search backward and center cursor in middle of screen' })
+
+-- Horizontal line movement
 map({ 'n', 'v' }, '<leader>jh', '^', { desc = 'Go to beginning of line' })
 map({ 'n', 'v' }, '<leader>jl', '$', { desc = 'Go to end of line' })
+map({ 'n', 'v' }, '<leader>jb', '%', { desc = 'Find the next (curly/square) bracket on this line and go to its match' })
+map({ 'n' }, '<leader>de', 'd$', { desc = 'Delete to end of line' })
 
--- Line movement - top, bottom
+-- Vertical line movement
 map({ 'n', 'v' }, '<leader>ju', 'gg', { desc = 'Go to top of file' })
 map({ 'n', 'v' }, '<leader>jn', '<S-g>', { desc = 'Go to bottom of file' })
 
--- Move Lines --------------------------------------------------------------------------------------------
--- Move down
-map('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
+-- Move line down
+map({ 'n' }, '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
 map('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
 map('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move down' })
 
--- Move up
-map('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
+-- Move line up
+map({ 'n' }, '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
 map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
 map('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move up' })
 
 -- highlights under cursor
 if vim.fn.has 'nvim-0.9.0' == 1 then
-    map('n', '<leader>ui', vim.show_pos, { desc = 'Inspect Pos' })
+    map({ 'n' }, '<leader>ui', vim.show_pos, { desc = 'Inspect Pos' })
 end
 
 -- terminal
@@ -166,42 +150,45 @@ vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
 --------------------------------------------------------------------------------------------
 -- Open things -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------
--- Open Lazygit
-map('n', '<leader>gg', '<cmd>:LazyGit<cr>', { desc = 'Open LazyGit' })
--- Open Plugin Manager
-map('n', '<C-p>', '<cmd>:Lazy<cr>', { desc = 'Open Lazy Plugin Manager' })
+map({ 'n' }, '<leader>gg', '<cmd>:LazyGit<cr>', { desc = 'Open LazyGit' })
+map({ 'n' }, '<C-p>', '<cmd>:Lazy<cr>', { desc = 'Open Lazy Plugin Manager' })
 
--- Diagnostic keymaps
-map('n', '<leader>dj', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic message' })
-map('n', '<leader>dk', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic message' })
+--------------------------------------------------------------------------------------------
+-- Diagnostics  --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
 
-map('n', '<leader>di', vim.diagnostic.open_float, { desc = 'Open floating [d]iagnostic info message' })
+map({ 'n' }, '<leader>dj', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic message' })
+map({ 'n' }, '<leader>dk', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic message' })
+map({ 'n' }, '<leader>di', vim.diagnostic.open_float, { desc = 'Open floating [d]iagnostic info message' })
+map({ 'n' }, '<leader>df', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = 'Trouble: Buffer/[f]ile [d]iagnostics' })
+map({ 'n' }, '<leader>da', '<cmd>Trouble diagnostics toggle<cr>', { desc = 'Trouble: [A]ll open Buffer [d]iagnostics' })
+-- map({ 'n' }, '<leader>os', '<cmd>Trouble symbols toggle focus=false<cr>', { desc = 'Trouble: [O]pen [s]ymbols diagnostics ' })
+-- map({ 'n' }, '<leader>ol', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', { desc = 'Trouble: [O]pen [L]SP diagnostics' })
 
-map('n', '<leader>df', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = 'Trouble: Buffer/[f]ile [d]iagnostics' })
-map('n', '<leader>da', '<cmd>Trouble diagnostics toggle<cr>', { desc = 'Trouble: [A]ll open Buffer [d]iagnostics' })
+--------------------------------------------------------------------------------------------
+-- Refactor  --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
 
-map('n', '<leader>os', '<cmd>Trouble symbols toggle focus=false<cr>', { desc = 'Trouble: [O]pen [s]ymbols diagnostics ' })
-map('n', '<leader>ol', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', { desc = 'Trouble: [O]pen [L]SP diagnostics' })
-
---- Code changes
-
-map('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Refactor: Rename variable' })
-map('x', '<leader>re', ':Refactor extract ', { desc = 'Refactor: Extract' })
-map('x', '<leader>rf', ':Refactor extract_to_file ', { desc = 'Refactor: Extract to file' })
-map('x', '<leader>rv', ':Refactor extract_var ', { desc = 'Refactor: Extract variable' })
+map({ 'n' }, '<leader>rn', vim.lsp.buf.rename, { desc = 'Refactor: Rename variable' })
+map({ 'x' }, '<leader>re', ':Refactor extract ', { desc = 'Refactor: Extract' })
+map({ 'x' }, '<leader>rf', ':Refactor extract_to_file ', { desc = 'Refactor: Extract to file' })
+map({ 'x' }, '<leader>rv', ':Refactor extract_var ', { desc = 'Refactor: Extract variable' })
 map({ 'n', 'x' }, '<leader>ri', ':Refactor inline_var', { desc = 'Refactor: Inline variable' })
-map('n', '<leader>rI', ':Refactor inline_func', { desc = 'Refactor: Inline function' })
-map('n', '<leader>rb', ':Refactor extract_block', { desc = 'Refactor: Extract block' })
-map('n', '<leader>rbf', ':Refactor extract_block_to_file', { desc = 'Refactor: extract block to file' })
+map({ 'n' }, '<leader>rI', ':Refactor inline_func', { desc = 'Refactor: Inline function' })
+map({ 'n' }, '<leader>rb', ':Refactor extract_block', { desc = 'Refactor: Extract block' })
+map({ 'n' }, '<leader>rbf', ':Refactor extract_block_to_file', { desc = 'Refactor: extract block to file' })
 
+--------------------------------------------------------------------------------------------
+-- Refactor  --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
 -- Code action
-map('n', '<leader>ka', vim.lsp.buf.code_action, { desc = 'Code action' })
+map({ 'n' }, '<leader>ka', vim.lsp.buf.code_action, { desc = 'Code action' })
 
 -- Twilight highlights current block and dims other code
-map('n', '<leader>kt', '<cmd>Twilight<cr>', { desc = 'Turn on Twilight' })
+map({ 'n' }, '<leader>kt', '<cmd>Twilight<cr>', { desc = 'Turn on Twilight' })
 
 -- Code folding
-map('n', '<leader>kf', 'za', { desc = 'Code folding' })
+map({ 'n' }, '<leader>kf', 'za', { desc = 'Code folding' })
 
 --Todo comments in telescope
-map('n', '<leader>lt', '<cmd>:TodoTelescope<cr>', { desc = 'Display all todo comments in telescope' })
+map({ 'n' }, '<leader>lt', '<cmd>:TodoTelescope<cr>', { desc = 'Display all todo comments in telescope' })
