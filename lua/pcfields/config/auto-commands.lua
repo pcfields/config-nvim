19baee56843f3799f65501a4cf4394b16b70d22c
Peter <pcfields@gmail.com>
2024-10-13 00:14:29 +0200

    refactor autocommand for buffer winbar

--- lua/pcfields/config/auto-commands.lua
+++ lua/pcfields/config/auto-commands.lua
@@ -24,32 +24,43 @@ vim.cmd(string.format([[highlight WinBar1 guifg=%s]], '#78a9ff'))
 vim.cmd(string.format([[highlight WinBar2 guifg=%s]], '#00ff00'))
 
 -- Function to get the full path and replace the home directory with ~
-local function get_winbar_path()
+local function get_formatted_file_path()
     local full_path = vim.fn.expand '%:p'
+
     return full_path:gsub(vim.fn.expand '$HOME', '~')
 end
 
 -- Function to get the number of open buffers using the :ls command
 local function get_buffer_count()
-    local buffers = vim.fn.execute 'ls'
+    local open_buffers = vim.fn.execute 'ls'
     local count = 0
+
     -- Match only lines that represent buffers, typically starting with a number followed by a space
-    for line in string.gmatch(buffers, '[^\r\n]+') do
+    for line in string.gmatch(open_buffers, '[^\r\n]+') do
         if string.match(line, '^%s*%d+') then
             count = count + 1
         end
     end
+
     return count
 end
 
 -- Function to update the winbar
 local function update_winbar()
-    local home_replaced = get_winbar_path()
-    local buffer_count = get_buffer_count()
+    local formatted_file_path = get_formatted_file_path()
+    local open_buffers_count = get_buffer_count()
+    local is_buffer_modified = vim.api.nvim_buf_get_option(0, 'modified')
+    local modified_buffer_icon = is_buffer_modified and ' 🟠 ' or ''
 
-    vim.opt.winbar = '%#WinBar1#%m ' .. '%#WinBar2#(' .. buffer_count .. ') ' .. '%#WinBar1#' .. home_replaced
+    vim.opt.winbar = table.concat {
+        modified_buffer_icon,
+        '%#WinBar2#(' .. open_buffers_count .. ') ',
+        '%#WinBar1#',
+        '' .. formatted_file_path,
+    }
 end
+
 -- Autocmd to update the winbar on BufEnter and WinEnter events
-vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter' }, {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter', 'BufWritePost', 'TextChanged', 'TextChangedI' }, {
     callback = update_winbar,
 })
