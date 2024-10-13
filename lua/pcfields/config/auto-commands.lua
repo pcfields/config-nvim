3abbc8184cdd867cc51c7eaabcf8b3ab7b2138b2
Peter <pcfields@gmail.com>
2024-10-13 09:38:27 +0200

    add icons for file changed

--- lua/pcfields/config/auto-commands.lua
+++ lua/pcfields/config/auto-commands.lua
@@ -4,7 +4,7 @@ local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = t
 
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-        vim.highlight.on_yank()
+        vim.highlight.on_yank { timeout = 300 }
     end,
     group = highlight_group,
     pattern = '*',
@@ -19,9 +19,9 @@ vim.filetype.add {
     },
 }
 -- Taken from https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/config/options.lua
--- Update Tab bar at the top
-vim.cmd(string.format([[highlight WinBar1 guifg=%s]], '#78a9ff'))
-vim.cmd(string.format([[highlight WinBar2 guifg=%s]], '#00ff00'))
+--  Create colors to use in winbar
+vim.cmd(string.format([[highlight FilePathColor guifg=%s]], '#78a9ff'))
+vim.cmd(string.format([[highlight BuffersCountColor guifg=%s]], '#00ff00'))
 
 -- Function to get the full path and replace the home directory with ~
 local function get_formatted_file_path()
@@ -45,22 +45,22 @@ local function get_buffer_count()
     return count
 end
 
--- Function to update the winbar
 local function update_winbar()
     local formatted_file_path = get_formatted_file_path()
     local open_buffers_count = get_buffer_count()
-    local is_buffer_modified = vim.api.nvim_buf_get_option(0, 'modified')
-    local modified_buffer_icon = is_buffer_modified and ' 🟠 ' or ''
+    local is_buffer_modified = vim.api.nvim_get_option_value('modified', { buf = 0 })
+
+    local modified_buffer_icon = is_buffer_modified and ' 🟠 ' or ' ⚫️ '
 
     vim.opt.winbar = table.concat {
         modified_buffer_icon,
-        '%#WinBar2#(' .. open_buffers_count .. ') ',
-        '%#WinBar1#',
+        '%#BuffersCountColor#(' .. open_buffers_count .. ') ',
+        '%#FilePathColor#',
         '' .. formatted_file_path,
     }
 end
 
--- Autocmd to update the winbar on BufEnter and WinEnter events
+-- Autocmd to update the winbar on events
 vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter', 'BufWritePost', 'TextChanged', 'TextChangedI' }, {
     callback = update_winbar,
 })
