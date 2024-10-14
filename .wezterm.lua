2b84352f81a5a292e7c8687bd67f08290ad70a56
Peter Fields <peterf@glidertech.com>
2024-10-14 09:18:35 +0100

    reduce wezterm background opacity

--- .wezterm.lua
+++ .wezterm.lua
@@ -135,7 +135,7 @@ config.default_prog = { os_shell }
 config.window_decorations = 'RESIZE|TITLE'
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.hide_tab_bar_if_only_one_tab = false
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.9
 config.font = wezterm.font {
     family = 'JetBrains Mono',
     weight = 'Medium',
