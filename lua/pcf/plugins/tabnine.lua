34106600566de064492ee35e951ad05e5ff49b44
Peter Fields <peterf@glidertech.com>
2024-12-11 12:47:31 +0000

    remove custom tabnine keymap

--- lua/pcf/plugins/tabnine.lua
+++ lua/pcf/plugins/tabnine.lua
@@ -21,20 +21,5 @@ return { -- Tabnine AI assistant
             log_file_path = nil, -- absolute path to Tabnine log file
             ignore_certificate_errors = false,
         }
-
-        -- You can set `accept_keymap` and `dismiss_keymap` to `false` to disable them
-        -- then you can create mappings using `require('tabnine.keymaps')`
-
-        --- Example integration with Tabnine and LuaSnip
-        --- falling back to inserting tab if neither has a completion
-        vim.keymap.set('i', '<tab>', function()
-            if require('tabnine.keymaps').has_suggestion() then
-                return require('tabnine.keymaps').accept_suggestion()
-            elseif require('luasnip').jumpable(1) then
-                return require('luasnip').jump(1)
-            else
-                return '<tab>'
-            end
-        end, { expr = true })
     end,
 }
