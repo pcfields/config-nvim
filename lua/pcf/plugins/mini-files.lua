046b9e715a89626e3841ee02c906af88af7a0b17
Peter Fields <peterf@glidertech.com>
2024-12-16 17:56:32 +0000

    setup mini.files plugin

--- lua/pcf/plugins/mini-files.lua
+++ lua/pcf/plugins/mini-files.lua
@@ -1,4 +1,10 @@
 -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-files.md
 -- Navigate and manipulate file system
 
-return { 'echasnovski/mini.files', version = '*' }
+return {
+    'echasnovski/mini.files',
+    version = '*',
+    config = function()
+        require('mini.files').setup()
+    end,
+}
