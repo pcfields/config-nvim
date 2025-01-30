328763adf4f4e84e98b8308598ab857d889475da
Peter Fields <peterf@glidertech.com>
2025-01-30 10:19:46 +0000

    refactor: utils map

--- lua/pcf/utils.lua
+++ lua/pcf/utils.lua
@@ -1,9 +1,21 @@
 local Utils = {}
 
 function Utils.map(mode, lhs, rhs, opts)
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
+    -- Initialize opts if it's not provided
+    if opts == nil then
+        opts = {}
+    end
 
+    -- Set default values for opts
+    if opts.silent == nil then
+        opts.silent = true
+    end
+
+    if opts.noremap == nil then
+        opts.noremap = true
+    end
+
+    -- Create the mapping
     vim.keymap.set(mode, lhs, rhs, opts)
 end
 
