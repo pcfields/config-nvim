53da683d55ae9c6bc9375ccf4a5c77da8725ef7e
Peter Fields <peterf@glidertech.com>
2025-02-01 13:04:47 +0000

    update: add treesitter select text objects

--- lua/pcf/plugins/editor/treesitter.lua
+++ lua/pcf/plugins/editor/treesitter.lua
@@ -21,6 +21,9 @@ return { -- Highlight, edit, and navigate code
                 'lua',
                 'tsx',
                 'typescript',
+                'elm',
+                'haskell',
+                'rust',
             },
 
             -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
@@ -41,50 +44,19 @@ return { -- Highlight, edit, and navigate code
                     node_decremental = '<M-space>',
                 },
             },
+            -- Enable syntax aware text objects
             textobjects = {
-                -- select = {
-                --     enable = true,
-                --     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                --     keymaps = {
-                --         -- You can use the capture groups defined in textobjects.scm
-                --         ['aa'] = '@parameter.outer',
-                --         ['ia'] = '@parameter.inner',
-                --         ['af'] = '@function.outer',
-                --         ['if'] = '@function.inner',
-                --         ['ac'] = '@class.outer',
-                --         ['ic'] = '@class.inner'
-                --     }
-                -- },
-                -- move = {
-                --     enable = true,
-                --     set_jumps = true, -- whether to set jumps in the jumplist
-                --     goto_next_start = {
-                --         [']m'] = '@function.outer',
-                --         [']]'] = '@class.outer'
-                --     },
-                --     goto_next_end = {
-                --         [']M'] = '@function.outer',
-                --         [']['] = '@class.outer'
-                --     },
-                --     goto_previous_start = {
-                --         ['[m'] = '@function.outer',
-                --         ['[['] = '@class.outer'
-                --     },
-                --     goto_previous_end = {
-                --         ['[M'] = '@function.outer',
-                --         ['[]'] = '@class.outer'
-                --     }
-                -- },
-                -- swap = {
-                --     swap_next = {
-                --         ['<leader>a'] = '@parameter.inner'
-                --     },
-                --     swap_previous = {
-                --         ['<leader>A'] = '@parameter.inner'
-                --     },
-                --     enable = true
-                -- }
-            },
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = '@class.inner',
+                    },
+                },
+            }
         }
 
         -- Setup rename open & closing html tag
