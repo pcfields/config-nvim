2c26caaf1e96fe3a151b058353fa3dde320f9807
Peter Fields <peterf@glidertech.com>
2025-06-03 18:01:57 +0100

    update lspconfig

--- lua/pcf/plugins/lsp/lsp-config.lua
+++ lua/pcf/plugins/lsp/lsp-config.lua
@@ -1,69 +1,61 @@
--- https://github.com/neovim/nvim-lspconfig
--- https://github.com/williamboman/mason-lspconfig.nvim
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim",          config = true },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "j-hui/fidget.nvim",                tag = "legacy", opts = {} },
+    { "saghen/blink.cmp" },
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
 
-return { -- LSP Configuration & Plugins
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		{ "williamboman/mason.nvim", config = true },
-		{ "williamboman/mason-lspconfig.nvim" },
-		{ "j-hui/fidget.nvim", tag = "legacy", opts = {} }, -- Useful status updates for LSP
-		{ "saghen/blink.cmp" },
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local lspconfig = require("lspconfig")
+    -- Initialize capabilities first
+    local capabilities = require("blink.cmp").get_lsp_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+    )
 
-		local function buffer_format()
-			vim.lsp.buf.format()
-		end
+    local on_attach = function(client, bufnr)
+      vim.api.nvim_buf_create_user_command(
+        bufnr,
+        "Format",
+        function()
+          vim.lsp.buf.format()
+        end,
+        { desc = "Format current buffer with LSP" })
+    end
 
-		--  This function gets run when an LSP connects to a particular buffer.
-		local on_attach = function(_, bufnr)
-			vim.api.nvim_buf_create_user_command(bufnr, "Format", buffer_format, { desc = "Format current buffer with LSP" })
-		end
+    -- Server configurations using new API
+    local language_servers = {
+      marksman = {},
+      jsonls = {},
+      cssls = {},
+      ts_ls = {},
+      eslint = {},
+      emmet_ls = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false }
+          }
+        },
+      }
+    }
 
-		--  Add any additional override configuration in the following tables. They will be passed to
-		--  the `settings` field of the server config. You must look up that documentation yourself.
-		local language_servers = {
-			marksman = {},
-			jsonls = {},
-			cssls = {},
-			ts_ls = {},
-			eslint = {},
-			emmet_ls = {},
-			lua_ls = {
-				Lua = {
-					workspace = {
-						checkThirdParty = false,
-					},
-					telemetry = {
-						enable = false,
-					},
-				},
-			},
-		}
+    -- Configure each server using native API
+    for server, config in pairs(language_servers) do
+      vim.lsp.config(server, vim.tbl_extend("force", {
+        capabilities = capabilities,
+        on_attach = on_attach
+      }, config))
+    end
 
-		-- code completion supports additional completion capabilities, so broadcast that to servers
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+    mason.setup()
 
-		mason.setup()
-
-		-- Ensure the language_servers are installed
-		mason_lspconfig.setup({
-			ensure_installed = vim.tbl_keys(language_servers),
-			automatic_installation = true,
-		})
-
-		mason_lspconfig.setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-					on_attach = on_attach,
-					settings = language_servers[server_name],
-				})
-			end,
-		})
-	end,
+    mason_lspconfig.setup({
+      ensure_installed = vim.tbl_keys(language_servers),
+      automatic_enable = true
+    })
+  end
 }
