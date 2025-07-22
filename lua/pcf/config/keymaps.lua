cdaa84a9ce3530225f332ab7c99cb51c896e34cb
Peter Fields <peterf@glidertech.com>
2025-07-22 14:28:56 +0100

    fix: go back to letter h for buffers

--- lua/pcf/config/keymaps.lua
+++ lua/pcf/config/keymaps.lua
@@ -78,18 +78,18 @@ map({ "n" }, "<leader>fg", "<cmd>Neotree git_status<cr>", { desc = "File git sta
 -- Buffers ----------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------
 
-map({ "n", "v", "s" }, "<leader>as", "<cmd>w<cr><esc>", { desc = "Save buffer" })
-map({ "n", "v", "s" }, "<leader>ae", "<cmd>wa<cr><esc>", { desc = "Save all buffers" })
+map({ "n", "v", "s" }, "<leader>hs", "<cmd>w<cr><esc>", { desc = "Save buffer" })
+map({ "n", "v", "s" }, "<leader>he", "<cmd>wa<cr><esc>", { desc = "Save all buffers" })
 
-map({ "n" }, "<leader>an", "<cmd>enew<cr>", { desc = "New buffer(file)" })
-map({ "n" }, "<leader>aq", close_buffer_and_keep_split, { desc = "Close buffer and keep split" })
-map({ "n" }, "<leader>ax", "<cmd>:close<cr>", { desc = "Close split window" })
-map({ "n" }, "<leader>au", "<cmd>e #<cr>", { desc = "Switch to last used buffer" })
-map({ "n" }, "<leader>ao", [[:%bdelete|edit #|bdelete #<CR>]], { desc = "Close all buffers except current one" })
-map({ "n" }, "<leader>ac", ":%y+<CR>", { desc = "Copy all text in buffer to clipboard" })
-map({ "n" }, "<leader>at", "gg<S-v>G", { desc = "Select all text in buffer" })
-map({ "n" }, "<leader>al", "<cmd>bnext<cr>", { desc = "Next buffer" })
-map({ "n" }, "<leader>ah", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+map({ "n" }, "<leader>hn", "<cmd>enew<cr>", { desc = "New buffer(file)" })
+map({ "n" }, "<leader>hq", close_buffer_and_keep_split, { desc = "Close buffer and keep split" })
+map({ "n" }, "<leader>hx", "<cmd>:close<cr>", { desc = "Close split window" })
+map({ "n" }, "<leader>hu", "<cmd>e #<cr>", { desc = "Switch to last used buffer" })
+map({ "n" }, "<leader>ho", [[:%bdelete|edit #|bdelete #<CR>]], { desc = "Close all buffers except current one" })
+map({ "n" }, "<leader>hc", ":%y+<CR>", { desc = "Copy all text in buffer to clipboard" })
+map({ "n" }, "<leader>ht", "gg<S-v>G", { desc = "Select all text in buffer" })
+map({ "n" }, "<leader>hj", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map({ "n" }, "<leader>hk", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
 
 --------------------------------------------------------------------------------------------
 -- Windows ---------------------------------------------------------------------------------
--- lua/pcf/plugins/ui/snacks.lua
+++ lua/pcf/plugins/ui/snacks.lua
@@ -30,7 +30,7 @@ return {
 		})
 
 		-- Buffers
-		map({ "n", "v" }, "<leader>aa", u.ViewOpenBuffers, { desc = "View Open Buffers" })
+		map({ "n", "v" }, "<leader>hh", u.ViewOpenBuffers, { desc = "View Open Buffers" })
 
 		-- Search
 		map({ "n", "v" }, "<leader>se", u.SearchGrep, { desc = "Search everywhere using Grep" })
