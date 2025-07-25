4624911a812e422b1b5d4d6e5ebf58806b2bc092
Peter Fields <peterf@glidertech.com>
2025-07-25 17:57:32 +0100

    change: adjust winbar colors

--- lua/pcf/utils/buffers.lua
+++ lua/pcf/utils/buffers.lua
@@ -8,6 +8,7 @@ local Icons = {
 local Colors = {
 	bright_green = "#00ff00",
 	pale_blue = "#78a9ff",
+	gray_light = "#4d5c7b",
 }
 
 -- Function to get the number of open buffers using the :ls command
@@ -26,15 +27,24 @@ function M.get_buffer_count()
 end
 
 function M.get_winbar_filename()
-	vim.api.nvim_set_hl(0, "FilePathColor", { fg = Colors.pale_blue })
-	vim.api.nvim_set_hl(0, "BuffersCountColor", { fg = Colors.bright_green, bold = true })
+	vim.api.nvim_set_hl(0, "FilenameColor", { fg = Colors.pale_blue })
+	vim.api.nvim_set_hl(0, "BrightGreenColor", { fg = Colors.bright_green, bold = true })
+	vim.api.nvim_set_hl(0, "FilePathDimColor", { fg = Colors.gray_light, bold = true })
 
 	local file_path = vim.api.nvim_eval_statusline("%f", {}).str
 	local open_buffers_count = M.get_buffer_count()
 	local is_buffer_modified = vim.api.nvim_eval_statusline("%m", {}).str == "[+]"
 	local modified_buffer_icon = is_buffer_modified and Icons.MODIFIED or Icons.UNMODIFIED
 
-	return "%#BuffersCountColor#" .. modified_buffer_icon .. "(" .. open_buffers_count .. ") " .. "%#FilePathColor#" .. "" .. file_path
+	local directory_path = vim.fn.fnamemodify(file_path, ":h") -- Directory (head, no filename)
+	local filename = vim.fn.fnamemodify(file_path, ":t")      -- Tail (filename only)
+
+	local open_buffers_count_formatted = "(" .. open_buffers_count .. ") "
+	local dimmed_directory_path = "%#FilePathColor#" .. directory_path .. "/"
+	local bright_filename = "%#BrightGreenColor#" .. filename
+
+	return "%#BrightGreenColor#" ..
+			modified_buffer_icon .. open_buffers_count_formatted .. dimmed_directory_path .. bright_filename
 end
 
 return M
