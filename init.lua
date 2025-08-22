841e7bd0e461c00a1374d1f4697a52c69e6df2a7
Peter Fields <pcfields@gmail.com>
2025-08-22 23:31:23 +0200

    refactor: reduce duplication in init.lua

--- init.lua
+++ init.lua
@@ -14,46 +14,52 @@ local plugin_manager = require("pcf.config.plugin-manager")
 -----------------------------------------------------
 -- Load plugins
 -----------------------------------------------------
+
+-- Helper function to reduce duplication
+local function plugin(path)
+	return require("pcf.plugins." .. path)
+end
+
 plugin_manager.setup({
-	require("pcf.plugins.themes.nightfox"), -- MAIN THEME
+	plugin("themes.nightfox"), -- MAIN THEME
 
-	require("pcf.plugins.ai.copilot"),
-	require("pcf.plugins.ai.copilot-chat"),
+	plugin("ai.copilot"),
+	plugin("ai.copilot-chat"),
 
-	require("pcf.plugins.completion.blink"),
+	plugin("completion.blink"),
 
-	require("pcf.plugins.debugging.dap"),
+	plugin("debugging.dap"),
 
-	require("pcf.plugins.editor.autopairs"),
-	require("pcf.plugins.editor.code-indentation"),
-	require("pcf.plugins.editor.comment"),
-	require("pcf.plugins.editor.flash"),
-	require("pcf.plugins.editor.local-highlight"),
-	require("pcf.plugins.editor.nvim-surround"),
-	require("pcf.plugins.editor.refactoring"),
-	require("pcf.plugins.editor.todo-comments"),
-	require("pcf.plugins.editor.treesitter"),
-	require("pcf.plugins.editor.trouble"),
-	require("pcf.plugins.editor.ufo"),
+	plugin("editor.autopairs"),
+	plugin("editor.code-indentation"),
+	plugin("editor.comment"),
+	plugin("editor.flash"),
+	plugin("editor.local-highlight"),
+	plugin("editor.nvim-surround"),
+	plugin("editor.refactoring"),
+	plugin("editor.todo-comments"),
+	plugin("editor.treesitter"),
+	plugin("editor.trouble"),
+	plugin("editor.ufo"),
 
-	require("pcf.plugins.git.diffview"),
-	require("pcf.plugins.git.gitsigns"),
+	plugin("git.diffview"),
+	plugin("git.gitsigns"),
 
-	require("pcf.plugins.lsp.format"),
-	require("pcf.plugins.lsp.lint"),
-	require("pcf.plugins.lsp.lsp-config"),
+	plugin("lsp.format"),
+	plugin("lsp.lint"),
+	plugin("lsp.lsp-config"),
 
-	require("pcf.plugins.navigation.mini-files"),
-	require("pcf.plugins.navigation.neo-tree"),
-	require("pcf.plugins.navigation.oil"),
+	plugin("navigation.mini-files"),
+	plugin("navigation.neo-tree"),
+	plugin("navigation.oil"),
 
-	require("pcf.plugins.testing.neo-test"),
+	plugin("testing.neo-test"),
 
-	require("pcf.plugins.ui.dressing"),
-	require("pcf.plugins.ui.lualine"),
-	require("pcf.plugins.ui.noice"),
-	require("pcf.plugins.ui.snacks"),
-	require("pcf.plugins.ui.which-key"),
+	plugin("ui.dressing"),
+	plugin("ui.lualine"),
+	plugin("ui.noice"),
+	plugin("ui.snacks"),
+	plugin("ui.which-key"),
 
-	require("pcf.plugins.utils.lazydev"),
+	plugin("utils.lazydev"),
 }, {})
