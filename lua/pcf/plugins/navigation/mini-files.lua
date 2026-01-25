e56912f53c29aca51dd31e0c54b6df43e6a46ff1
Peter Fields <pcfields@gmail.com>
2026-01-25 14:18:52 +0100

    change mini file keymap

--- lua/pcf/plugins/navigation/mini-files.lua
+++ lua/pcf/plugins/navigation/mini-files.lua
@@ -9,7 +9,7 @@ return {
 
 		require("mini.files").setup({})
 
-		map("n", "<leader>fm", function()
+		map("n", "<leader>fd", function()
 			require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
 		end, { desc = "Open mini.files (Directory of Current File)" })
 	end,
